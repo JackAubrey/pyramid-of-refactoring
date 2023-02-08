17f0aa77d81985ea4036aa77e3f80c3a879fc2f9
Danilo Cividin <danilo.cividin.ext@infocert.it>
2023-02-08 23:26:49 +0100

    5. Interpreter with Factories Module cleanup continued

---
+++ 1-Legacy-Search/src/main/java/pl/refactoring/interpreter/legacy/AndSpec.java
@@ -0,0 +1,15 @@
+package pl.refactoring.interpreter.legacy;
+
+public class AndSpec implements Spec{
+    private final Spec firstSpec;
+    private final Spec secondSpec;
+
+    public AndSpec(Spec firstSpec, Spec secondSpec) {
+        this.firstSpec = firstSpec;
+        this.secondSpec = secondSpec;
+    }
+
+    public boolean isSatisfiedBy(RealEstate estate) {
+        return firstSpec.isSatisfiedBy(estate) && secondSpec.isSatisfiedBy(estate);
+    }
+}
