6d9d46e4e09ee644ab8264e518cfc3d918de5e80
Danilo Cividin <danilo.cividin.ext@infocert.it>
2023-02-10 00:10:22 +0100

    Implemented PlacementSpec

--- 3-Emerging-Interpreter/src/main/java/pl/refactoring/interpreter/emerging/RealEstateFinder.java
+++ 3-Emerging-Interpreter/src/main/java/pl/refactoring/interpreter/emerging/RealEstateFinder.java
@@ -7,6 +7,7 @@ package pl.refactoring.interpreter.emerging;
 import pl.refactoring.interpreter.emerging.specs.AndSpec;
 import pl.refactoring.interpreter.emerging.specs.BelowAreaSpec;
 import pl.refactoring.interpreter.emerging.specs.MaterialSpec;
+import pl.refactoring.interpreter.emerging.specs.PlacementSpec;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -16,42 +17,61 @@ import java.util.stream.Collectors;
 public class RealEstateFinder {
     private List<RealEstate> repository;
 
+    /**
+     *
+     * @param repository
+     */
     public RealEstateFinder(List<RealEstate> repository) {
         this.repository = repository;
     }
 
+    /**
+     *
+     * @param spec
+     * @return
+     */
     public List<RealEstate> bySpec(Spec spec) {
         return repository.stream()
                 .filter(spec::isSatisfiedBy)
                 .collect(Collectors.toList());
     }
 
+    /**
+     *
+     * @param maxBuildingArea
+     * @return
+     */
     public List<RealEstate> byBelowArea(float maxBuildingArea){
         return bySpec(new BelowAreaSpec(maxBuildingArea));
     }
 
+    /**
+     *
+     * @param material
+     * @return
+     */
     public List<RealEstate> byMaterial(EstateMaterial material){
         return bySpec(new MaterialSpec(material));
 
     }
 
+    /**
+     * 
+     * @param material
+     * @param maxBuildingArea
+     * @return
+     */
     public List<RealEstate> byMaterialBelowArea(EstateMaterial material, float maxBuildingArea){
-        Spec materialSpec = new MaterialSpec(material);
-        Spec belowAreaSpec = new BelowAreaSpec(maxBuildingArea);
-
-        return bySpec(new AndSpec(materialSpec, belowAreaSpec));
+        return bySpec(new AndSpec(new MaterialSpec(material), new BelowAreaSpec(maxBuildingArea)));
     }
 
+    /**
+     *
+     * @param placement
+     * @return
+     */
     public List<RealEstate> byPlacement(EstatePlacement placement){
-        List<RealEstate> foundRealEstates = new ArrayList<>();
-
-        Iterator<RealEstate> estates = repository.iterator();
-        while (estates.hasNext()) {
-            RealEstate estate = estates.next();
-            if (estate.getPlacement().equals(placement))
-                foundRealEstates.add(estate);
-        }
-        return foundRealEstates;
+        return bySpec(new PlacementSpec(placement));
     }
 
     public List<RealEstate> byAvoidingPlacement(EstatePlacement placement){
@@ -60,7 +80,7 @@ public class RealEstateFinder {
         Iterator<RealEstate> estates = repository.iterator();
         while (estates.hasNext()) {
             RealEstate estate = estates.next();
-            if (! estate.getPlacement().equals(placement))
+            if (!new PlacementSpec(placement).isSatisfiedBy(estate))
                 foundRealEstates.add(estate);
         }
         return foundRealEstates;
@@ -96,9 +116,10 @@ public class RealEstateFinder {
         Iterator<RealEstate> estates = repository.iterator();
         while (estates.hasNext()) {
             RealEstate estate = estates.next();
-            if (estate.getType().equals(type) && estate.getPlacement().equals(placement) && estate.getMaterial().equals(material))
+            if (estate.getType().equals(type) && new PlacementSpec(placement).isSatisfiedBy(estate) && estate.getMaterial().equals(material))
                 foundRealEstates.add(estate);
         }
         return foundRealEstates;
     }
+
 }
