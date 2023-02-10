fd0ae7948061b63bd3e956b680299d907a7d7d6a
Danilo Cividin <danilo.cividin.ext@infocert.it>
2023-02-10 22:59:13 +0100

    Removed some comments

--- 3-Emerging-Interpreter/src/main/java/pl/refactoring/interpreter/emerging/RealEstateFinder.java
+++ 3-Emerging-Interpreter/src/main/java/pl/refactoring/interpreter/emerging/RealEstateFinder.java
@@ -10,104 +10,51 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RealEstateFinder {
-    private List<RealEstate> repository;
+    private final List<RealEstate> repository;
 
-    /**
-     *
-     * @param repository
-     */
     public RealEstateFinder(List<RealEstate> repository) {
         this.repository = repository;
     }
 
-    /**
-     *
-     * @param spec
-     * @return
-     */
     public List<RealEstate> bySpec(Spec spec) {
         return repository.stream()
                 .filter(spec::isSatisfiedBy)
                 .collect(Collectors.toList());
     }
 
-    /**
-     *
-     * @param maxBuildingArea
-     * @return
-     */
     public List<RealEstate> byBelowArea(float maxBuildingArea){
         return bySpec(new BelowAreaSpec(maxBuildingArea));
     }
 
-    /**
-     *
-     * @param material
-     * @return
-     */
     public List<RealEstate> byMaterial(EstateMaterial material){
         return bySpec(new MaterialSpec(material));
 
     }
 
-    /**
-     * 
-     * @param material
-     * @param maxBuildingArea
-     * @return
-     */
     public List<RealEstate> byMaterialBelowArea(EstateMaterial material, float maxBuildingArea){
         return bySpec(new AndSpec(new MaterialSpec(material), new BelowAreaSpec(maxBuildingArea)));
     }
 
-    /**
-     *
-     * @param placement
-     * @return
-     */
     public List<RealEstate> byPlacement(EstatePlacement placement){
         return bySpec(new PlacementSpec(placement));
     }
 
-    /**
-     *
-     * @param placement
-     * @return
-     */
     public List<RealEstate> byAvoidingPlacement(EstatePlacement placement){
         return bySpec(new NotSpec(new PlacementSpec(placement)));
     }
 
-    /**
-     *
-     * @param minArea
-     * @param maxArea
-     * @return
-     */
     public List<RealEstate> byAreaRange(float minArea, float maxArea){
         return bySpec(new AreaRangeSpec(minArea, maxArea));
     }
 
-    /**
-     *
-     * @param type
-     * @return
-     */
     public List<RealEstate> byType(EstateType type){
         return bySpec(new TypeSpec(type));
     }
 
-    /**
-     *
-     * @param type
-     * @param placement
-     * @param material
-     * @return
-     */
     public List<RealEstate> byTypePlacementMaterial(EstateType type, EstatePlacement placement, EstateMaterial material){
-        TypeSpec typeSpec = new TypeSpec(type);
-        PlacementSpec placementSpec = new PlacementSpec(placement);
-        MaterialSpec materialSpec = new MaterialSpec(material);
+        Spec typeSpec = new TypeSpec(type);
+        Spec placementSpec = new PlacementSpec(placement);
+        Spec materialSpec = new MaterialSpec(material);
 
         return bySpec(new AndSpec(typeSpec, placementSpec, materialSpec));
     }
